a839e2c2147af886a39324a6e84ea2a7bc7a1048
everlearner-bee <erastusandedo@gmail.com>
2022-11-24 22:55:14 +0300

    Create dictionaries.py

---
+++ dictionaries.py
@@ -0,0 +1,18 @@
+#Dictionaries, they are defined by key : value pairs
+
+student = {'name':'Erastus','Adm':'ci/00030/017'}
+print(student['Adm'])
+
+# working with dictionaries
+
+student = {'name':'Erastus','Adm':'ci/00030/017'}
+
+#  adding new values to dictionaries
+
+student['age'] = 22
+student['nationality'] = 'kenyan'
+
+print(student)
+
+# modyfying values in dictionaries
+
